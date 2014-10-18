8f2e2387cf91b48e8ad6cde2283db67bad178ebe
Seqian Wang <wang030@live.com>
2014-10-18 16:09:08 -0400

    Update

--- scripts/JasonStart.py
+++ scripts/JasonStart.py
@@ -1,6 +1,7 @@
 import numpy as np
 import nibabel
 import os
+import glob
 
 DataDir = '/Users/jason/Desktop/BrainHack/brainimages'
 file = '113_NZ_DWI_FA_reg2STD.nii'
@@ -16,16 +17,11 @@ MaskData = MaskImg.get_data()
 MaskIndices = MaskData.nonzero()
 NVoxelsMask = MaskData[MaskIndices].size
 
+for datatType in ['*VBM_GM*' '*DWI_FA*' '*DWI_MD*' '*DWI_S0' '*MTR*']
+    pass
 
 for i in glob.glob('*DWI_FA*.nii'):
-	img = nibabel.load(os.path.join(DataDir,i))
-	data = img.get_data()
+    img = nibabel.load(os.path.join(DataDir,i))
+    data = img.get_data()
 
 data = data[MaskIndices]
-
-
-
-
-
-
-
