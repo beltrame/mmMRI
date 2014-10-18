ea3623cc044b85029a2d3e1c242a606730a40559
cesine <cesine@yahoo.com>
2014-10-18 19:58:54 -0400

    using sample code from brain browser surface demo

--- client/js/directives/brainbrowser.js
+++ client/js/directives/brainbrowser.js
@@ -10,8 +10,9 @@
  */
 angular.module("app").directive("brainbrowser", function() {
 
-  var controller = function($scope, $location, $timeout) {
+  var controller = function($scope, $timeout) {
     console.log("Loading brain browser");
+
 
   };
   controller.$inject = ["$scope", "$location", "$timeout"];
@@ -25,10 +26,45 @@ angular.module("app").directive("brainbrowser", function() {
     //   authentication: "=json"
     // },
     controller: controller,
-    link: function postLink() {},
-    priority: 0,
-    replace: true,
-    controllerAs: "stringAlias"
+    link: function postLink(scope, element) {
+      console.log("putting element in scope so it can be used by brain browser");
+      scope.element = element[0];
+
+      BrainBrowser.config.set("worker_dir", "js/brainbrowser/workers/");
+
+      // Custom configuration for the Surface Viewer demo app.
+      BrainBrowser.config.set("model_types.freesurferasc.format_hint", 'You can use <a href="http://surfer.nmr.mgh.harvard.edu/fswiki/mris_convert" target="_blank">mris_convert</a> to convert your binary surface files into .asc format.');
+      BrainBrowser.config.set("intensity_data_types.freesurferasc.format_hint", 'You can use <a href="http://surfer.nmr.mgh.harvard.edu/fswiki/mris_convert" target="_blank">mris_convert</a> to convert your binary surface files into .asc format.');
+
+      BrainBrowser.config.set("color_maps", [{
+        name: "Spectral",
+        url: "color-maps/spectral.txt",
+      }, {
+        name: "Thermal",
+        url: "color-maps/thermal.txt",
+      }, {
+        name: "Gray",
+        url: "color-maps/gray-scale.txt",
+      }, {
+        name: "Blue",
+        url: "color-maps/blue.txt",
+      }, {
+        name: "Green",
+        url: "color-maps/green.txt",
+      }]);
+
+      if (!scope.element) {
+        console.warn("the element is missing, not starting the brain browser");
+        return;
+      }
+
+      /////////////////////////////////////
+      // Start running the Surface Viewer
+      /////////////////////////////////////
+      window.viewer = BrainBrowser.SurfaceViewer.start(scope.element, function(viewer) {
+
+      });
+    }
   };
   return directiveDefinitionObject;
 });
