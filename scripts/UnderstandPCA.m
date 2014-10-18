2f2791abbb7f9c42c820302e36c549f7a1252b01
steffejr <steffener@gmail.com>
2014-10-18 16:48:48 -0400

    additional PCA notes

--- scripts/UnderstandPCA.m
+++ scripts/UnderstandPCA.m
@@ -3,33 +3,42 @@ N = 100; % Number of subjects
 M = 20000; % Number of voxels
 X = randn(N,M); % random data
 
-Z = X*X'; % Create a covariance matrix of the data
 
+Z = X*X'; % Create a covariance matrix of the data
+% But for this the means need to be removed from X, this is the
+% standardization part
+
+% Creation of this Z matrix is needed so that the following step is
+% tractable. The eigen decomposition is now done on a small matrix.
 [W S] = svd(Z); % perform singular value decomposition of the data;
-% W are the eigen images and S are the squared eigenvalues
+% W are the eigen vectors and S are the squared eigenvalues
 
 % Plot the eigenvalues
 figure(1)
 bar(diag(sqrt(S)))
+
+
+% Get the eigen images back. This brings the eigenvectors back into the
+% image space
+V = X'*W;
 
 % Create the score matrix also called the subejct scaling factors
 % These are one number per person per eigen image. These values represent
 % the degree to which each person "expresses" the eigenimage. These scores
 % can be used to determine the weighted combination that predicts
 % something. They can also be used with other tools to determine different
-% groups. For instance, you may want to determine how 
+% groups. 
+T = X*V;
 
+% From the T matrix you can do fancy things to create a new vector B which
+% is a weighted combination of the eigenvectors. Note, that some of these
+% eigenvectors can be set to ZERO to ignore them.
 
-T = X*W;
+% Create the weighted  ombination image as:
 
-% Once the weights of the T vectors are created call it: B then the
-% corresponding image can be created.
-
-BImage = W*B;
+BImage = V*B;
 
 % This will give you a single image which is a weighted combination of the
 % eigen images that best describe your covariate.
 % As a example you may use
 
-
-A1 = u;
