48a411933384928515f568cde28fbfebbae76a89
cesine <cesine@yahoo.com>
2014-10-19 09:13:34 -0400

    fixes  #17 added a matrix view and binding

--- client/js/controllers/MainCtrl.js
+++ client/js/controllers/MainCtrl.js
@@ -3,6 +3,23 @@ angular.module('app')
         $scope.dataTransformOrPipelines = dataTransformOrPipelineData.getAll();
         $scope.dataTransformOrPipeline = null;
 
+        $scope.matrix = [
+            [, "subject1", "subject2", "subject3", "subject4", "subject5", "subject6", "subject7", "subject8", "subject9", "subject10", "subject11", "subject12"],
+            ["subject1", 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ["subject2", 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ["subject3", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ["subject4", 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            ["subject5", 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+            ["subject6", 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            ["subject7", 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            ["subject8", 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            ["subject9", 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            ["subject10", 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            ["subject11", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            ["subject12", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        ];
+        
+
         $scope.useLogScaleForBubbleSize = false;
         $scope.toogleButtonScale = function() {
             $scope.useLogScaleForBubbleSize = !$scope.useLogScaleForBubbleSize;
