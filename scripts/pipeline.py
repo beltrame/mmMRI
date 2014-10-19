8ffa18af19f99a944b8cb596b076a64cd5208b18
Giovanni Beltrame <giovanni.beltrame@polymtl.ca>
2014-10-19 15:56:45 -0400

    Pipeline for demo

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -6,26 +6,43 @@ Created on Sun Oct 19 13:55:44 2014
 """
 
 import sys, getopt
+import pickle
+import sklearn.preprocessing
+from sklearn.decomposition import PCA
 
 def main(argv):
-   maskfile = ''
-   components = ''
-   try:
+    maskfile = ''
+    n_components = 26
+    try:
       opts, args = getopt.getopt(argv,"hm:c:",["maskfile=","components="])
-      if len(argv)<2: raise getopt.GetoptError("USAGE:")
-   except getopt.GetoptError:
+    except getopt.GetoptError:
       print 'pipeline.py -m <maskfile> -c <# components>'
       sys.exit(2)
-   for opt, arg in opts:
+    for opt, arg in opts:
       if opt == '-h':
          print 'Usage:\n\tpipeline.py -m <maskfile> -c <# components>'
          sys.exit()
       elif opt in ("-m", "--maskfile"):
          maskfile = arg
       elif opt in ("-c", "--components"):
-         components = arg
-   print 'Input file is ', maskfile
-   print 'Components ', components
+         n_components = arg
+    #print 'Input file is ', maskfile
+    #print 'Components ', n_components
+
+    dataset = pickle.load(open('dataset.pickle','rb'))
+
+    # PCA
+    # Standardize the dataset
+    sklearn.preprocessing.scale(dataset)
+
+    # Fit the model
+    pca = PCA(n_components=n_components, whiten=True).fit(dataset)
+    
+    # Transform the dataset
+    dataPCA = pca.transform(dataset)
+
+    # LEARNING
+    print dataPCA
 
 if __name__ == "__main__":
    main(sys.argv[1:])
