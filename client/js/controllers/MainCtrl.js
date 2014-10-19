27390e6e9433a59228be4e30ed13c45ecfc2d5da
cesine <cesine@yahoo.com>
2014-10-19 11:34:15 -0400

    added nvd3 bar chart and scatter plot fixes #20

--- client/js/controllers/MainCtrl.js
+++ client/js/controllers/MainCtrl.js
@@ -33,6 +33,126 @@ angular.module('app')
             visibility: 0
         }];
 
+        $scope.barchart1 = {
+            options: {
+                chart: {
+                    type: 'discreteBarChart',
+                    height: 450,
+                    margin: {
+                        top: 20,
+                        right: 20,
+                        bottom: 60,
+                        left: 55
+                    },
+                    x: function(d) {
+                        return d.label;
+                    },
+                    y: function(d) {
+                        return d.value;
+                    },
+                    showValues: true,
+                    valueFormat: function(d) {
+                        return d3.format(',.4f')(d);
+                    },
+                    transitionDuration: 500,
+                    xAxis: {
+                        axisLabel: 'X Axis'
+                    },
+                    yAxis: {
+                        axisLabel: 'Y Axis',
+                        axisLabelDistance: 30
+                    }
+                },
+            },
+            data: [{
+                key: "Cumulative Return",
+                values: [{
+                    "label": "A",
+                    "value": -29.765957771107
+                }, {
+                    "label": "B",
+                    "value": 0
+                }, {
+                    "label": "C",
+                    "value": 32.807804682612
+                }, {
+                    "label": "D",
+                    "value": 196.45946739256
+                }, {
+                    "label": "E",
+                    "value": 0.19434030906893
+                }, {
+                    "label": "F",
+                    "value": -98.079782601442
+                }, {
+                    "label": "G",
+                    "value": -13.925743130903
+                }, {
+                    "label": "H",
+                    "value": -5.1387322875705
+                }]
+            }]
+        };
+
+
+        /* Random Data Generator (took from nvd3.org) */
+        function generateData(groups, points) { //# groups,# points per group
+            var data = [],
+                shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
+                random = d3.random.normal();
+
+            for (var i = 0; i < groups; i++) {
+                data.push({
+                    key: 'Group ' + i,
+                    values: [],
+                    slope: Math.random() - .01,
+                    intercept: Math.random() - .5
+                });
+
+                for (var j = 0; j < points; j++) {
+                    data[i].values.push({
+                        x: random(),
+                        y: random(),
+                        size: Math.random(),
+                        shape: shapes[j % 6]
+                    });
+                }
+            }
+            return data;
+        }
+        $scope.scatterplot1 = {
+            options: {
+                chart: {
+                    type: 'scatterPlusLineChart',
+                    height: 450,
+                    color: d3.scale.category10().range(),
+                    scatter: {
+                        onlyCircles: false
+                    },
+                    showDistX: true,
+                    showDistY: true,
+                    tooltipContent: function(key) {
+                        return '<h3>' + key + '</h3>';
+                    },
+                    transitionDuration: 350,
+                    xAxis: {
+                        axisLabel: 'X Axis',
+                        tickFormat: function(d) {
+                            return d3.format('.02f')(d);
+                        }
+                    },
+                    yAxis: {
+                        axisLabel: 'Y Axis',
+                        tickFormat: function(d) {
+                            return d3.format('.02f')(d);
+                        },
+                        axisLabelDistance: 30
+                    }
+                }
+            },
+            data: generateData(4, 40)
+        };
+
 
         $scope.runScript = function(scriptname) {
             console.warn("security hole, this should not permit execution of unknown scripts.");
@@ -54,7 +174,7 @@ angular.module('app')
 
         var stopBenchmarksWatch = $scope.$watch('dataTransformOrPipelines.length', function(length) {
             if (length) {
-                if ($location.url().indexOf("/dataTransformOrPipeline/") == 0) {
+                if ($location.url().indexOf("/dataTransformOrPipeline/") === 0) {
                     var dataTransformOrPipelineName = $location.url().match(/^\/dataTransformOrPipeline\/(.+)/);
                     if (dataTransformOrPipelineName) {
                         dataTransformOrPipelineName = dataTransformOrPipelineName[1];
