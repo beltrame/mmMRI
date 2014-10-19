cd841f0c9a666aa1613c597743e476c36ca92962
Wang030 <seqian@outlook.com>
2014-10-19 14:50:58 -0400

    Organized the script a bit better with functions

--- scripts/JasonStart.py
+++ scripts/JasonStart.py
@@ -2,42 +2,34 @@ import numpy.ma as ma
 import nibabel
 import os
 import glob
-
-DataDir = '/home/user03/data/brainimages/'
-maskFile = '/home/user03/data/masks/auditorynetwork_mask.nii.gz'
-
-if False:
-    DataDir = '/Users/jason/Desktop/BrainHack/brainimages'
-    file = '113_NZ_DWI_FA_reg2STD.nii'
-    img = nibabel.load(os.path.join(DataDir, file))
-    data = img.get_data()
+import sklearn
+import sys
 
 def getMask(dataFile, maskFile):
-    #MaskDir = DataDir
-    #MaskDir = '/Users/jason/Desktop/BrainHack/masks'
-    #MaskFile = 'auditorynetwork_mask.nii'
-    #MaskImg = nibabel.load(os.path.join(MaskDir,MaskFile))
-    #MaskData = MaskImg.get_data()
-
-    #MaskIndices = MaskData.nonzero()
-    #NVoxelsMask = MaskData[MaskIndices].size
-
     maskFile = nibabel.load(maskFile).get_data()
-    # dataFile[maskFile == 0] = 0  # For maintaining 3D array integrity
     dataFile = dataFile[maskFile == 1]  # For generating a vector
     return dataFile
 
-matrix = {}
-for dataType in ['*VBM_GM*_z.nii', '*DWI_FA*_z.nii', '*DWI_MD*_z.nii', '*DWI_S0*_z.nii', '*MTR*_z.nii']:
-    for imageFile in glob.glob(DataDir + dataType):
-        #img = nibabel.load(os.path.join(DataDir,imageFile))
-        subjectID = imageFile.split('/')[-1][0:3]
-	if subjectID == "106": #Ignoring subject 106
-		continue
-        data = nibabel.load(imageFile).get_data()
-        data_masked = getMask(data, maskFile)
-        matrix[subjectID] = data_masked
+def createMatrix(DataDir, maskFile):
+	matrix = {}
+	for dataType in ['*VBM_GM*_z.nii', '*DWI_FA*_z.nii', '*DWI_MD*_z.nii', '*DWI_S0*_z.nii', '*MTR*_z.nii']:
+		for imageFile in glob.glob(DataDir + dataType):
+			subjectID = imageFile.split('/')[-1][0:3]
+			if subjectID == "106": #Ignoring subject 106
+				continue
+        		data = nibabel.load(imageFile).get_data()
+        		data_masked = getMask(data, maskFile)
+	matrix[subjectID] = data_masked
+	return ma.array(matrix.items(), dtype=dtype)
 
-#return ma.array(matrix.items(), dtype=dtype)
+if __name__ == "__main__":
+	if len(sys.argv)<2:
+		print "Usage: JasonStart.py <path to .nii.gz files> <mask file>"
+		sys.exit(0)
+	DataDir = sys.argv[1]
+	maskFile = sys.argv[2]
 
-pass
+	n_components = 24
+	
+	dataArray = createMatrix(DataDir, maskFile)
+	pca = sklearn.decomposition.RandomizedPCA(n_components=n_components).fit(dataArray)
